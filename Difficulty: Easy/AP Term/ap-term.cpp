a6fca83d6569719b8cc208b74f28a2b2ac290dac
Suryansh10 <suryanshforcommunity@gmail.com>
2026-07-14 12:41:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/AP Term/ap-term.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, d, n;
+    cin >> a >> d >> n;
+
+    // code here
+    int ans;
+    ans=( a + (n-1)*d);
+    cout<<ans;
+
+    return 0;
+}
