1132dbee074607466065d091838d32402a760dd2
Suryansh10 <suryanshforcommunity@gmail.com>
2026-07-21 22:56:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Compute a/b/compute-ab.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    double compute(int a, int b) {
+        float c = a;
+        float d = b;
+       float e = c/d;
+       return e;
+        // code here
+    
+    }
+};
