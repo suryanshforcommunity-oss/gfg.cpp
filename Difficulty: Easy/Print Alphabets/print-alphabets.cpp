bd2b1d94fc88460297e6c4cb47daebce27de4eb7
Suryansh10 <suryanshforcommunity@gmail.com>
2026-07-28 23:38:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Alphabets/print-alphabets.cpp
@@ -0,0 +1,12 @@
+// function should print the alphabets
+// from c1 to c2 (space separated)
+// No need to print the new line
+// The new line will be printed by the
+// driver's code.
+
+void alphabets(char c1, char c2) {
+    // code here
+    for(char c =c1; c<=c2;c++){
+    cout<<c<<" ";
+    }
+}
